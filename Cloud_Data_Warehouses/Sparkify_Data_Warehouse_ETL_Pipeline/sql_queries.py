b563153a2de7e91ac36c0b9eb9cfab206701ea2b
Anna <anna.natalia.dutkiewicz@gmail.com>
2020-06-05 21:31:57 +0200

    sql_queries update

--- Cloud_Data_Warehouses/Sparkify_Data_Warehouse_ETL_Pipeline/sql_queries.py
+++ Cloud_Data_Warehouses/Sparkify_Data_Warehouse_ETL_Pipeline/sql_queries.py
@@ -7,17 +7,20 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+    CREATE TABLE staging_events (
+        
+    );
 """)
 
 staging_songs_table_create = ("""
